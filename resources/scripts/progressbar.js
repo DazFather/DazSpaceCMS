bf4253f6cf7383b63cf469ccdb112cd9e72d6808
DazFather <the6daz6@gmail.com>
2021-09-30 00:15:44 +0200

    Improved UI for homepage and articles

--- resources/scripts/progressbar.js
+++ resources/scripts/progressbar.js
@@ -1,5 +1,4 @@
 function UpdateProgress(){
-    console.log('someone is using me !')
     done = document.body.scrollTop;
     max = (document.body.scrollHeight - document.body.offsetHeight);
     document.getElementById('progressbar').style.width = done / max * 100 + '%';
